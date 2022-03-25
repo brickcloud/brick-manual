3ce465fde8b8f9f218e9420a84111da296dcde9d
brickcloud <81146067+brickcloud@users.noreply.github.com>
2022-03-25 17:07:30 +0900

    Update conf.py

--- brick/docs/source/conf.py
+++ brick/docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Brick'
+copyright = '2021, CloudHUB'
+author = 'igkyu Choi'
 
 release = '0.1'
 version = '0.1.0'
